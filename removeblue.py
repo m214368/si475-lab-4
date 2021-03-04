b08013951b315a982d1d7928fe6dba7d0a259ee1
Administrator <administrator@kinetic.kinetic>
2021-03-04 15:22:58 -0500

    purple added

--- removeblue.py
+++ removeblue.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 img = cv2.imread('blue.png')
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-outhsv = cv2.inRange(hsv, np.array([82, 20, 10]), np.array([207,255,235]))
+outhsv = cv2.inRange(hsv, np.array([210/2, 15, 20]), np.array([240/2,255,255]))
 img[:, :, 1] = np.bitwise_or(img[:, :, 1], outhsv)
 cv2.imwrite('noblueaugment.jpg', img)
 cv2.imwrite('noblue.jpg', outhsv)
