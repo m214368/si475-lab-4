60361d2f8221bd3d05e857ed8ce2169ddde746fa
Administrator <administrator@kinetic.kinetic>
2021-03-11 11:29:50 -0500

    Fixed value in remove blue- use 220 instead of 210, change in main file

--- removeblue.py
+++ removeblue.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 img = cv2.imread('blue.png')
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-outhsv = cv2.inRange(hsv, np.array([210/2, 15, 20]), np.array([240/2,255,255]))
+outhsv = cv2.inRange(hsv, np.array([220/2, 15, 20]), np.array([240/2,255,255]))
 img[:, :, 1] = np.bitwise_or(img[:, :, 1], outhsv)
 cv2.imwrite('noblueaugment.jpg', img)
 cv2.imwrite('noblue.jpg', outhsv)
--- removeyellow.py
+++ removeyellow.py
@@ -3,6 +3,14 @@ import numpy as np
 img = cv2.imread('imagefiles/balloon_yellow.png')
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 outhsv = cv2.inRange(hsv, np.array([50/2, 20, 10]), np.array([70/2,255,235]))
+
+# blue colored
+#img[:, :, 0] = np.bitwise_or(img[:, :, 0], outhsv)
+
+# green colored
 img[:, :, 1] = np.bitwise_or(img[:, :, 1], outhsv)
-cv2.imwrite('calculated/noyellowaugment.jpg', img)
-cv2.imwrite('calculated/noyellowmap.jpg', outhsv)
+
+# orange/red colored
+#img[:, :, 2] = np.bitwise_or(img[:, :, 2], outhsv)
+cv2.imwrite('noyellowaugment.jpg', img)
+cv2.imwrite('noyellowmap.jpg', outhsv)
