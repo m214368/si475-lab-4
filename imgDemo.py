04f74c253381664e9a4bc3a23f4ec475da07c9c4
Administrator <administrator@kinetic.kinetic>
2021-03-15 09:39:13 -0400

    done

--- imgDemo.py
+++ imgDemo.py
@@ -28,20 +28,89 @@ def pid_speed(kp, ki, kd, error, old_error, error_list):
     return to_return
 
 def hunt(color):
-    speed_limit = 2 # speed limit
+    speed_limit = 1 # speed limit
     turn_limit = .2 # turn speed limit
     colormap = {"blue":[220,240],"green":[130,160],"purple":[290,320],"red":[-10,10],"yellow":[50,70]}
     bot = np.array([colormap[color][0]/2, 20, 10])
     top = np.array([colormap[color][1]/2,255,235])
 
-    rate = rospy.Rate(10)
+    rate = rospy.Rate(30)
 
+    #size = spin(r,top,bot,turn_limit,rate)
+    size = 6
+    
+    r.drive(angSpeed=0, linSpeed=0)
+    print("done with spin")
+    print("largest blob: ",size)
+
+    # loop until at position
+    old_error = 0
+
+    while True:
+        image = r.getImage()
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        mapimage = cv2.inRange(hsv, bot, top)
+        augimage = image
+        augimage[:, :, 1] = np.bitwise_or(image[:, :, 1], mapimage)
+        cv2.imshow('augmented',augimage)
+        cv2.waitKey(1)
+        # current pos
+        current_pos = r.getPositionTup()
+        current_angle = current_pos[2]
+        
+        height, width = mapimage.shape[0:2]
+        total = cv2.countNonZero(mapimage)
+        width = width//2 # integer division
+        halfLeft = mapimage[:,:width]
+        left = cv2.countNonZero(halfLeft)
+        halfRight = mapimage[:,width:]
+        right = cv2.countNonZero(halfRight)
+        print ("total: "+str(total)+" left: "+str(left)+" right: "+str(right))
+
+        # calculate angle speed and lin speed drive
+        lin_speed = speed_limit
+        
+        # if less than 1/3rd of the original blob size found keep turning left
+        # but with 0 lin_speed
+        last_seen = 1 # 1 for left, -1 for right
+        if (total < size/3):
+            error = 100 * last_seen
+            lin_speed = 0
+        else:
+            error = (left - right)/total
+            last_seen = np.sign(error)
+
+        if (total > 30000):
+            lin_speed = 0
+            if (abs(error) < 10):
+                r.drive(angSpeed=0, linSpeed=0)
+                print("done")
+                return
+
+        # speed
+        ang_speed = pid_speed(.2, .01, .001, error, old_error, error_list)
+
+
+        if (ang_speed > turn_limit):
+            ang_speed = turn_limit
+
+        ang_speed = ang_speed + (lin_speed/speed_limit)*np.sign(ang_speed)
+
+        r.drive(angSpeed=ang_speed, linSpeed=lin_speed)
+        print('speed: ' + str(ang_speed) + ' ' + str(lin_speed))
+
+        # set old values
+        old_error=error
+        rate.sleep()
+        print(' ')
+
+def spin(r,top,bot,turn_limit,rate):
     # initial spin to find largest color blob
     cur_pos = r.getPositionTup()
     init_ang = cur_pos[2]
+    
     size = 0
-
-    # changed to false when spin is done
+    
     stillSpin = True
     pastQuarter = False
 
@@ -84,59 +153,8 @@ def hunt(color):
             stillSpin = False
 
         rate.sleep()
-
-    r.drive(angSpeed=0, linSpeed=0)
-    print("done with spin")
-    print("largest blob: ",size)
-
-    # loop until at position
-    old_error = 0
-
-    while True:
-        image = r.getImage()
-        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        mapimage = cv2.inRange(hsv, bot, top)
-        augimage = image
-        augimage[:, :, 1] = np.bitwise_or(image[:, :, 1], mapimage)
-        cv2.imshow('augmented',augimage)
-        cv2.waitKey(1)
-        # current pos
-        current_pos = r.getPositionTup()
-        current_angle = current_pos[2]
         
-        height, width = mapimage.shape[0:2]
-        total = cv2.countNonZero(mapimage)
-        width = width//2 # integer division
-        halfLeft = mapimage[:,:width]
-        left = cv2.countNonZero(halfLeft)
-        halfRight = mapimage[:,width:]
-        right = cv2.countNonZero(halfRight)
-        print ("total: "+str(total)+" left: "+str(left)+" right: "+str(right))
-
-        # calculate angle speed and lin speed drive
-        error = left - right
-
-        lin_speed = speed_limit
-        
-        # if less than 1/3rd of the original blob size found keep turning right
-        if (total < size/3):
-            error = -size/2
-            lin_speed = 0
-
-        # speed
-        ang_speed = pid_speed(.0001, -.0001, -.0001, error, old_error, error_list)
-
-
-        if (ang_speed > turn_limit):
-            ang_speed = turn_limit
-
-        r.drive(angSpeed=ang_speed, linSpeed=lin_speed)
-        print('speed: ' + str(ang_speed) + ' ' + str(lin_speed))
-
-        # set old values
-        old_error=error
-        rate.sleep()
-        print(' ')
+    return size
 
 def Blob(): 
     # https://learnopencv.com/blob-detection-using-opencv-python-c/
@@ -172,6 +190,4 @@ def Blob():
         detector = cv2.SimpleBlobDetector_create(params)
 
 color = raw_input("What color to hunt:")
-while True:
-    hunt(color)
-
+hunt(color)
